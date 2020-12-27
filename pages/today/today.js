9e4f0d64e9a9020bafb5f9c666ec8ff51f0de7ae
mr-chen01 <279939269@qq.com>
2020-12-27 17:43:54 +0800

    12.27

--- pages/today/today.js
+++ pages/today/today.js
@@ -28,7 +28,6 @@ Page({
     that.getLevel()
     that.memberRoom()
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -61,7 +60,6 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
   },
 
   /**
